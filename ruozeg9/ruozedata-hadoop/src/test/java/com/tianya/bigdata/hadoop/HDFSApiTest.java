0f246c7dee4024edc35866e2399d297bbc6122cd
tianyafu <futy@huawangtech.com>
2020-07-06 15:43:53 +0800

    feat:增加ruozedata-hadoop项目

---
+++ ruozeg9/ruozedata-hadoop/src/test/java/com/tianya/bigdata/hadoop/HDFSApiTest.java
@@ -0,0 +1,19 @@
+package com.tianya.bigdata.hadoop;
+
+import org.junit.Test;
+
+/**
+ * HDFS API测试用例
+ *
+ * 编程入口点
+ *
+ */
+public class HDFSApiTest {
+
+
+    @Test
+    public void mkdir() throws Exception{
+
+
+    }
+}
