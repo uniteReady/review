34ce7e6d2148111157fb67ba1ea1bbbbd9f77c25
tianyafu1991 <1193835093@qq.com>
2020-08-09 14:59:23 +0800

    feat:创建ruozedata-hadoop-project项目

---
+++ ruozeg9/ruoze-hadoop-project/ruozedata-log-server/src/main/java/com/tianya/ruozedatalogserver/RuozedataLogServerApplication.java
@@ -0,0 +1,13 @@
+package com.tianya.ruozedatalogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuozedataLogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuozedataLogServerApplication.class, args);
+    }
+
+}
---
+++ ruozeg9/ruoze-hadoop-project/ruozedata-log-server/src/test/java/com/tianya/ruozedatalogserver/RuozedataLogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianya.ruozedatalogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuozedataLogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
